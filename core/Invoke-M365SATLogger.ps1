00b369b79d1e553ad7ad6c041b72fcbd4dc23f2c
Leonardo van de Weteringh <leonardo.vandeweteringh@asterict.nl>
2025-03-05 13:25:25 +0100

    Added different validation for Windows Linux for logging properly

--- core/Invoke-M365SATLogger.ps1
+++ core/Invoke-M365SATLogger.ps1
@@ -1,16 +1,17 @@
 function Invoke-M365SATLogger
 {
 	param(
-		[string]$RootDirectory,
-		[string]$OS
+		[string]$RootDirectory
 	)
 	
 	if ($RootDirectory.Contains('\')){
+		# Dirty-way of checking Windows
 		Write-Host $RootDirectory
 		Start-Logger -FilePath "$RootDirectory\log\$($DateNow)_M365SAT.log" -Console -MinimumLevel Warning
 		Write-WarningLog "Program Started!"
 	}
 	elseif ($RootDirectory.Contains('/')) {
+		# Dirty-way of checking Linux
 		Write-Host $RootDirectory
 		Start-Logger -FilePath "$RootDirectory/log/$($DateNow)_M365SAT.log" -Console -MinimumLevel Warning
 		Write-WarningLog "Program Started!"
